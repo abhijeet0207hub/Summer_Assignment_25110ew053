64e14890216ecdb10434329a8cd879302795b145
garv <abhisinghbaghel007@gmail.com>
2026-06-21 23:31:53 +0530

    Day-21 uploaded

---
+++ Day-21/question81.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int length = 0;
+    
+    
+    for (char c : str) {
+        length++;
+    }
+
+    cout << "The length of the string is: " << length << endl;
+
+    return 0;
+}
---
+++ Day-21/question83.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int vowels = 0, consonants = 0;
+
+    for (char c : str) {
+        
+        if (c >= 'A' && c <= 'Z') {
+            c = c + 32;
+        }
+        
+        
+        if (c >= 'a' && c <= 'z') {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    cout << "Vowels: " << vowels << endl;
+    cout << "Consonants: " << consonants << endl;
+
+    return 0;
+}
